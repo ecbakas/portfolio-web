67ade263d68c2e2d806401218eff25cf47c4d359
Ertuğrulcan Bakas <ecbakas@gmail.com>
2026-02-11 22:34:18 +0300

    fix:navbar active

--- components/molecules/navbar.tsx
+++ components/molecules/navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { CircleUser, House, Library } from "lucide-react";
-import { Link } from "@/lib/navigation";
+import { Link, usePathname } from "@/lib/navigation";
 
 import {
   NavigationMenu,
@@ -10,7 +10,6 @@ import {
 } from "@/components/ui/navigation-menu";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { usePathname } from "next/navigation";
 import { useTranslations } from "next-intl";
 import HireMe from "./hire-me";
 import { ThemeToggle } from "./theme-toggle";
@@ -36,29 +35,31 @@ const navbarItems = [
 
 export default function Navbar() {
   const t = useTranslations("Navbar");
-  const active = usePathname();
-  console.log(active);
+  const activePath = usePathname();
+
   return (
     <NavigationMenu className="sticky top-0 z-50 min-h-16 w-full max-w-full rounded-lg border bg-white p-2 px-3 shadow-xl dark:bg-black">
       <NavigationMenuList className="">
-        {navbarItems.map((item) => (
-          <NavigationMenuItem key={item.key} asChild>
-            <Button
-              asChild
-              variant={"ghost"}
-              size={"icon"}
-              className={cn(
-                active === item.href
-                  ? "bg-gray-100 dark:bg-zinc-900"
-                  : "text-zinc-400",
-              )}
-            >
-              <Link href={item.href} title={t(item.key)}>
-                <item.icon className="h-10" />
-              </Link>
-            </Button>
-          </NavigationMenuItem>
-        ))}
+        {navbarItems.map((item) => {
+          return (
+            <NavigationMenuItem key={item.key} asChild>
+              <Button
+                asChild
+                variant={"ghost"}
+                size={"icon"}
+                className={cn(
+                  activePath === item.href
+                    ? "bg-gray-100 dark:bg-zinc-900"
+                    : "text-zinc-400",
+                )}
+              >
+                <Link href={item.href} title={t(item.key)}>
+                  <item.icon className="h-10" />
+                </Link>
+              </Button>
+            </NavigationMenuItem>
+          );
+        })}
       </NavigationMenuList>
 
       <LanguageSwitcher className="ml-auto border-none shadow-none" />
